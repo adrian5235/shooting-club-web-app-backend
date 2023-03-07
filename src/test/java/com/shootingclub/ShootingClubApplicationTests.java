dad922a2eb4c1a024984bfd8eb34c089214b872d
adrian <adrian@localhost>
2023-03-07 23:10:35 +0100

    Initial commit

---
+++ src/test/java/com/shootingclub/ShootingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shootingclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
